83d726e491e83584b7ac2c6f41a252d4892af10c
UygarAksaz <uygaraksaz@outlook.com>
2025-02-05 15:51:31 -0500

    Final Linting changes made

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -14,7 +14,7 @@ def initiate():
     for data in car_make_data:
         car_make_instances.append(
             CarMake.objects.create(name=data['name'],
-                                    description=data['description']))
+                                   description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -111,6 +111,6 @@ def initiate():
     ]
 
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], 
+        CarModel.objects.create(name=data['name'],
                                 car_make=data['car_make'],
                                 type=data['type'], year=data['year'])
